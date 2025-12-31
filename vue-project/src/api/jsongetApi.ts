ab45d2a8eccd0f2374bba775b088fc73aa0efc59
igada <iga.dai0226@gmail.com>
2025-12-31 16:02:33 +0900

    jsongetApiをvercelに対応

--- vue-project/src/api/jsongetApi.ts
+++ vue-project/src/api/jsongetApi.ts
@@ -1,9 +1,15 @@
-//srcフォルダ内からserverフォルダのapiを叩く
+// //srcフォルダ内からserverフォルダのapiを叩く
+// export async function jsonget() {
+//   const res = await fetch('/api/jsonget', {
+//     method: 'GET',
+//     headers: { Accept: 'application/json' }, // 期待する返り値を伝える
+//   });
+//   if (!res.ok) throw new Error(`HTTP ${res.status}`);
+//   return await res.json()
+// }
+
+const API_BASE = import.meta.env.VITE_API_URL ?? ''
 export async function jsonget() {
-  const res = await fetch('/api/jsonget', {
-    method: 'GET',
-    headers: { Accept: 'application/json' }, // 期待する返り値を伝える
-  });
-  if (!res.ok) throw new Error(`HTTP ${res.status}`);
-  return await res.json()
+  const res = await fetch(`${API_BASE}/api/jsonget`)
+  return res.json()
 }
