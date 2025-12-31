fe2d92976c6535f3e892d73da89fa4eb418d1565
igada <iga.dai0226@gmail.com>
2025-12-31 21:00:15 +0900

    apiのフェッチ先の書き方を変えました

--- src/api/jsongetApi.ts
+++ src/api/jsongetApi.ts
@@ -8,8 +8,9 @@
 //   return await res.json()
 // }
 
-const API_BASE = "https://myvuepageexpress.vercel.app"
+// const API_BASE = "https://myvuepageexpress.vercel.app"
+
 export async function jsonget() {
-  const res = await fetch(`${API_BASE}/api/jsonget`)
+  const res = await fetch(`https://myvuepageexpress.vercel.app/api/jsonget`)
   return res.json()
 }
